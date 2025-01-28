dfaaef2b138aa3f95decdb504ee47010a06c26fe
Zach Boone <zach.boone@fulfil.com>
2025-01-27 23:28:47 -0800

    test db, makefile

---
+++ Makefile
@@ -0,0 +1,48 @@
+##@ Help
+
+# The help target prints out all targets with their descriptions organized
+# beneath their categories. The categories are represented by '##@' and the
+# target descriptions by '##'. The awk commands is responsible for reading the
+# entire set of makefiles included in this invocation, looking for lines of the
+# file as xyz: ## something, and then pretty-format the target and help. Then,
+# if there's a line with ##@ something, that gets pretty-printed as a category.
+# More info on the usage of ANSI control characters for terminal formatting:
+# https://en.wikipedia.org/wiki/ANSI_escape_code#SGR_parameters
+# More info on the awk command:
+# http://linuxcommand.org/lc3_adv_awk.php
+.PHONY: help
+help: ## Shows the help
+	@awk 'BEGIN {FS = ":.*##"; printf "\nUsage:\n  make \033[36m<target>\033[0m\n"} /^[\/%.a-zA-Z_0-9-]+:.*?##/ { printf "  \033[36m%-30s\033[0m %s\n", $$1, $$2 } /^##@/ { printf "\n\033[1m%s\033[0m\n", substr($$0, 5) } ' $(MAKEFILE_LIST)
+
+##@ Development
+
+.PHONY: run
+run: node_modules ## Run the app locally
+	npm run dev
+
+node_modules: package-lock.json ## Install node modules
+	npm ci
+
+.PHONY: lint
+lint: ## Check ESLint for any errors
+	npx eslint ./src/**/*.ts
+
+##@ Testing
+
+.PHONY: test
+test: node_modules test-db test-db-migrate ## Run tests
+	npx dotenv -e .env.test -- npm test
+
+.PHONY: test-db
+test-db: ## Start test database
+	docker compose up -d
+
+.PHONY: test-db-clean
+test-db-clean: ## Stop and remove the test database
+	docker compose down
+
+.PHONY: test-db-migrate
+test-db-migrate: test-db ## Apply existing migrations to the database
+	@echo "Waiting for the database to be ready..."
+	@until docker exec pinewood-derby-db pg_isready -U testuser; do sleep 1; done
+	npx dotenv -e .env.test -- npx prisma migrate dev
---
+++ prisma/migrations/20250128065257_init/migration.sql
@@ -0,0 +1,81 @@
+-- CreateTable
+CREATE TABLE "car" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "den_id" INTEGER NOT NULL,
+    "owner" TEXT NOT NULL,
+    "number" SMALLINT NOT NULL,
+    "superlative" TEXT,
+    "name" TEXT,
+
+    CONSTRAINT "car_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "den" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "dirby_id" INTEGER NOT NULL,
+
+    CONSTRAINT "den_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "derby" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "time" TIMESTAMP(6) NOT NULL,
+    "location_name" TEXT NOT NULL,
+
+    CONSTRAINT "derby_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "heat" (
+    "id" BIGSERIAL NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "den_id" INTEGER NOT NULL,
+    "lane_1_car_id" INTEGER,
+    "lane_2_car_id" INTEGER,
+    "lane_3_car_id" INTEGER,
+    "lane_4_car_id" INTEGER,
+    "lane_5_car_id" INTEGER,
+    "lane_6_car_id" INTEGER,
+    "lane_1_car_time" SMALLINT,
+    "lane_2_car_time" SMALLINT,
+    "lane_3_car_time" SMALLINT,
+    "lane_4_car_time" SMALLINT,
+    "lane_5_car_time" SMALLINT,
+    "lane_6_car_time" SMALLINT,
+    "raced_at" TIMESTAMP(6),
+
+    CONSTRAINT "heat_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "car" ADD CONSTRAINT "car_den_id_fkey" FOREIGN KEY ("den_id") REFERENCES "den"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "den" ADD CONSTRAINT "den_dirby_id_fkey" FOREIGN KEY ("dirby_id") REFERENCES "derby"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_den_id_fkey" FOREIGN KEY ("den_id") REFERENCES "den"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_1_car_id_fkey" FOREIGN KEY ("lane_1_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_2_car_id_fkey" FOREIGN KEY ("lane_2_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_3_car_id_fkey" FOREIGN KEY ("lane_3_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_4_car_id_fkey" FOREIGN KEY ("lane_4_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_5_car_id_fkey" FOREIGN KEY ("lane_5_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "heat" ADD CONSTRAINT "heat_lane_6_car_id_fkey" FOREIGN KEY ("lane_6_car_id") REFERENCES "car"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
