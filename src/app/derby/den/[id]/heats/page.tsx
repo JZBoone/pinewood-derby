15899c9bac59197512973d09eb145a269f74449d
Zach Boone <zach.boone@fulfil.com>
2025-01-29 01:24:02 -0800

    copy heat id to clipboard

--- src/app/derby/den/[id]/heats/page.tsx
+++ src/app/derby/den/[id]/heats/page.tsx
@@ -95,36 +95,59 @@ interface HeatProps {
 function Heat({ heat, carsById, heatNumber }: HeatProps) {
   return (
     <div className="text-2xl">
-      <h2 className="den-name font-bold mb-2 mt-4">Heat #{heatNumber}</h2>
+      <h2
+        className="den-name font-bold mb-2 mt-4 cursor-pointer"
+        onClick={() => navigator.clipboard.writeText(heat.id.toString())}
+      >
+        Heat #{heatNumber}
+      </h2>
       <div>
         Lane 1:{' '}
         {heat.lane_1_car_id &&
           `#${carsById[heat.lane_1_car_id]?.number} - ${carsById[heat.lane_1_car_id]?.owner}`}
+        {heat.lane_1_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_1_car_time}</span>
+        )}
       </div>
       <div>
         Lane 2:{' '}
         {heat.lane_2_car_id &&
           `#${carsById[heat.lane_2_car_id]?.number} - ${carsById[heat.lane_2_car_id]?.owner}`}
+        {heat.lane_2_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_2_car_time}</span>
+        )}
       </div>
       <div>
         Lane 3:{' '}
         {heat.lane_3_car_id &&
           `#${carsById[heat.lane_3_car_id]?.number} - ${carsById[heat.lane_3_car_id]?.owner}`}
+        {heat.lane_3_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_3_car_time}</span>
+        )}
       </div>
       <div>
         Lane 4:{' '}
         {heat.lane_4_car_id &&
           `#${carsById[heat.lane_4_car_id]?.number} - ${carsById[heat.lane_4_car_id]?.owner}`}
+        {heat.lane_4_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_4_car_time}</span>
+        )}
       </div>
       <div>
         Lane 5:{' '}
         {heat.lane_5_car_id &&
           `#${carsById[heat.lane_5_car_id]?.number} - ${carsById[heat.lane_5_car_id]?.owner}`}
+        {heat.lane_5_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_5_car_time}</span>
+        )}
       </div>
       <div>
         Lane 6:{' '}
         {heat.lane_6_car_id &&
           `#${carsById[heat.lane_6_car_id]?.number} - ${carsById[heat.lane_6_car_id]?.owner}`}
+        {heat.lane_6_car_time && (
+          <span className="text-red-500"> - Time: {heat.lane_6_car_time}</span>
+        )}
       </div>
     </div>
   );
