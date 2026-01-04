1a1eab6a0f9bffe47759837cf31bf3394374e43a
Zach Boone <zach.boone@fulfil.com>
2026-01-04 12:39:09 -0800

    attempt to fix 500 error

--- src/app/api/derby/route.ts
+++ src/app/api/derby/route.ts
@@ -1,8 +1,8 @@
-import { NextResponse } from 'next/server';
 import { getAllDerbies } from '@/api-biz/derby';
 import { GetDerbiesResponse } from '@/lib/derby';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const derbies = await getAllDerbies();
   const response: GetDerbiesResponse = { derbies: derbies };
   return NextResponse.json(response);
