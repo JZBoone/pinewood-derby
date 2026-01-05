623f68d2b04e159e049a68a5ca254b5c9a3bb8a9
Zach Boone <jzachb@gmail.com>
2026-01-04 16:04:34 -0800

    install pg

--- src/app/api/derby/route.ts
+++ src/app/api/derby/route.ts
@@ -1,8 +1,8 @@
 import { getAllDerbies } from '@/api-biz/derby';
 import { GetDerbiesResponse } from '@/lib/derby';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const derbies = await getAllDerbies();
   const response: GetDerbiesResponse = { derbies: derbies };
   return NextResponse.json(response);
