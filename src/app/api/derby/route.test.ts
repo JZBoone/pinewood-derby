27d49c568f97679bc8a289afe5745d4b4142ceb6
J. Zach Boone <jzachb@gmail.com>
2026-01-25 12:56:44 -0800

    Improvements (#4)
    * pin postgres
    
    * create derby API
    
    * script wip
    
    ---------
    
    Co-authored-by: Zach Boone <zach.boone@fulfil.com>

--- src/app/api/derby/route.test.ts
+++ src/app/api/derby/route.test.ts
@@ -1,6 +1,7 @@
 import { db } from '@/api-biz/db';
 import { DateTime } from 'luxon';
-import { GET } from './route';
+import { NextRequest } from 'next/server';
+import { GET, POST } from './route';
 
 describe('GET /api/derby', () => {
   beforeEach(async () => {
@@ -17,5 +18,34 @@ describe('GET /api/derby', () => {
     expect(res.status).toBe(200);
     const json = await res.json();
     expect(json.derbies.length).toBe(1);
+  });
+});
+
+describe('POST /api/derby', () => {
+  test('it creates a new derby', async () => {
+    const time = DateTime.now().plus({ days: 3 }).toUTC().toISO();
+    const location_name = 'New Location';
+
+    const request = new NextRequest('http://localhost/api/derby', {
+      method: 'POST',
+      body: JSON.stringify({ time, location_name }),
+    });
+
+    const res = await POST(request);
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json.id).toBeDefined();
+    expect(json.created_at).toBeDefined();
+    expect(json.location_name).toBe(location_name);
+  });
+
+  test('it returns a 400 for an invalid payload', async () => {
+    const request = new NextRequest('http://localhost/api/derby', {
+      method: 'POST',
+      body: JSON.stringify({ time: new Date() }),
+    });
+
+    const res = await POST(request);
+    expect(res.status).toBe(400);
   });
 });
