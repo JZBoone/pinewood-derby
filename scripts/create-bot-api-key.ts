bf6278ee9bb9ead0c1f609f5c38fbc7b5b24874b
fulfilzach <zach.boone@fulfil.com>
2026-02-02 22:22:39 -0800

    fix

--- scripts/create-bot-api-key.ts
+++ scripts/create-bot-api-key.ts
@@ -1,14 +1,14 @@
 import { auth } from '@/lib/auth';
 
-// run with npx dotenv -e .env -- npx tsx ./scripts/create-race-interface-api-key.ts
+// run with npx dotenv -e .env -- npx tsx ./scripts/create-bot-api-key.ts
 
 async function createApiKey() {
   const data = await auth.api.createApiKey({
     body: {
-      name: '',
+      name: 'race-interface-bot',
       expiresIn: null,
-      userId: '',
-      prefix: '',
+      userId: '67',
+      prefix: 'race-interface-bot',
     },
   });
 
